c8572daae2a3ad0105dc3fd9ec257402c87d9105
HerrNilsen-K <typnilsen@gmail.com>
2021-05-13 11:55:01 +0200

    GLarray is done

--- renderer/GL/GLarray.hpp
+++ renderer/GL/GLarray.hpp
@@ -5,4 +5,45 @@
 #ifndef TETRIS_GLARRAY_HPP
 #define TETRIS_GLARRAY_HPP
 
+#include <GL/glew.h>
+
+class GLarray {
+public:
+    GLarray();
+
+    void bind();
+
+    //Uses float
+    void data(uint32_t start, uint32_t end, uint32_t offset);
+
+    void enable(uint8_t index);
+
+    ~GLarray();
+
+private:
+    uint32_t m_array = 0;
+};
+
+GLarray::GLarray() {
+    glGenVertexArrays(1, &m_array);
+}
+
+void GLarray::bind() {
+    glBindVertexArray(m_array);
+}
+
+void GLarray::data(uint32_t start, uint32_t end, uint32_t offset) {
+    bind();
+    glVertexAttribPointer(start, end, GL_FLOAT, false, end * sizeof(float), offset);
+}
+
+void GLarray::enable(uint8_t index) {
+    glEnableVertexAttribArray(index);
+}
+
+GLarray::~GLarray() {
+    glDeleteVertexArrays(1, &m_array);
+    glBindVertexArray(0);
+}
+
 #endif //TETRIS_GLARRAY_HPP
